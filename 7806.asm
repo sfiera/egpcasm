76ac0fde29fe3cb7bf4b81dac583b38a657deda1
Chris Pickel <sfiera@twotaled.com>
2025-10-13 21:23:18 +0900

    Reorder 7806.asm to match documentation

--- 7806.asm
+++ 7806.asm
@@ -90,127 +90,97 @@
 }
 
 #ruledef {
-    db {b: bytes} => b
-    dw {w: words} => w
-
-    nop => $00
-
-    ret => $08
-    rets => $18
-    reti => $62
-
-    jb => $73
-
-    stm => $19
-    daa => $61
-
-    ei  => $4820
-    di  => $4824
-    clc => $482A
-    stc => $482B
-    pex => $482D
-    ral => $4830
-    rar => $4831
-    rld => $4838
-    rrd => $4839
-    per => $483C
-
-    mov a, {reg: reg_bcdehl} => 0x0 @ 0b1 @ reg
+    ; 8-Bit Data Transfer
     mov {reg: reg_bcdehl}, a => 0x1 @ 0b1 @ reg
-    mov a, {port: mov_a_port} => 0x4cc @ port
+    mov a, {reg: reg_bcdehl} => 0x0 @ 0b1 @ reg
     mov {port: mov_a_port}, a => 0x4dc @ port
+    mov a, {port: mov_a_port} => 0x4cc @ port
     mov {reg: reg_vabcdehl}, [{addr: u16}] => $706 @ 0b1 @ reg @ le(addr)
     mov [{addr: u16}], {reg: reg_vabcdehl} => $707 @ 0b1 @ reg @ le(addr)
     mvi {reg: reg_abcdehl}, {value: u8} => $6 @ 0b1 @ reg @ value
-
-    lxi {reg: inx_reg}, {value: u16} => reg @ $4 @ le(value)
-    inx {reg: inx_reg} => reg @ $2
-    dcx {reg: inx_reg} => reg @ $3
-    ldax [{reg: ldax_reg}] => $2 @ 0b1 @ reg
-    stax [{reg: ldax_reg}] => $3 @ 0b1 @ reg
-
-    ldaw [{addr: hi_addr}] => $28 @ addr
     staw [{addr: hi_addr}] => $38 @ addr
+    ldaw [{addr: hi_addr}] => $28 @ addr
+    stax [{reg: ldax_reg}] => $3 @ 0b1 @ reg
+    ldax [{reg: ldax_reg}] => $2 @ 0b1 @ reg
 
-    inr {reg: reg_inr} => $4 @ reg
-    dcr {reg: reg_inr} => $5 @ reg
-    inrw [{addr: hi_addr}] => $20 @ addr
-    dcrw [{addr: hi_addr}] => $30 @ addr
+    ; 16-Bit Data Transfer
+    sbcd [{addr: u16}] => $701E @ le(addr)
+    sded [{addr: u16}] => $702E @ le(addr)
+    shld [{addr: u16}] => $703E @ le(addr)
+    sspd [{addr: u16}] => $700E @ le(addr)
+    lbcd [{addr: u16}] => $701F @ le(addr)
+    lded [{addr: u16}] => $702F @ le(addr)
+    lhld [{addr: u16}] => $703F @ le(addr)
+    lspd [{addr: u16}] => $700F @ le(addr)
+    push {reg: push_reg} => $48 @ reg @ $e
+    pop {reg: push_reg} => $48 @ reg @ $f
+    lxi {reg: inx_reg}, {value: u16} => reg @ $4 @ le(value)
 
-    ana     {reg: reg_vbcdehl}, a => $600 @ 0b1 @ reg
-    xra     {reg: reg_vbcdehl}, a => $601 @ 0b0 @ reg
-    ora     {reg: reg_vbcdehl}, a => $601 @ 0b1 @ reg
-    addnc   {reg: reg_vbcdehl}, a => $602 @ 0b0 @ reg
-    gta     {reg: reg_vbcdehl}, a => $602 @ 0b1 @ reg
-    subnb   {reg: reg_vbcdehl}, a => $603 @ 0b0 @ reg
-    lta     {reg: reg_vbcdehl}, a => $603 @ 0b1 @ reg
-    add     {reg: reg_vbcdehl}, a => $604 @ 0b0 @ reg
-    adc     {reg: reg_vbcdehl}, a => $605 @ 0b0 @ reg
-    sub     {reg: reg_vbcdehl}, a => $606 @ 0b0 @ reg
-    nea     {reg: reg_vbcdehl}, a => $606 @ 0b1 @ reg
-    sbb     {reg: reg_vbcdehl}, a => $607 @ 0b0 @ reg
-    eqa     {reg: reg_vbcdehl}, a => $607 @ 0b1 @ reg
+    ; Arithmetic
+    add     {reg: reg_vbcdehl}, a   => $604 @ 0b0 @ reg
+    add     a, {reg: reg_vabcdehl}  => $60C @ 0b0 @ reg
+    addx    [{reg: ldax_reg}]       => $70C @ 0b0 @ reg
+    adc     {reg: reg_vbcdehl}, a   => $605 @ 0b0 @ reg
+    adc     a, {reg: reg_vabcdehl}  => $60D @ 0b0 @ reg
+    adcx    [{reg: ldax_reg}]       => $70D @ 0b0 @ reg
+    sub     {reg: reg_vbcdehl}, a   => $606 @ 0b0 @ reg
+    sub     a, {reg: reg_vabcdehl}  => $60E @ 0b0 @ reg
+    subx    [{reg: ldax_reg}]       => $70E @ 0b0 @ reg
+    sbb     {reg: reg_vbcdehl}, a   => $607 @ 0b0 @ reg
+    sbb     a, {reg: reg_vabcdehl}  => $60F @ 0b0 @ reg
+    sbbx    [{reg: ldax_reg}]       => $70F @ 0b0 @ reg
+    addnc   {reg: reg_vbcdehl}, a   => $602 @ 0b0 @ reg
+    addnc   a, {reg: reg_vabcdehl}  => $60A @ 0b0 @ reg
+    addncx  [{reg: ldax_reg}]       => $70A @ 0b0 @ reg
+    subnb   {reg: reg_vbcdehl}, a   => $603 @ 0b0 @ reg
+    subnb   a, {reg: reg_vabcdehl}  => $60B @ 0b0 @ reg
+    subnbx  [{reg: ldax_reg}]       => $70B @ 0b0 @ reg
+    ana     {reg: reg_vbcdehl}, a   => $600 @ 0b1 @ reg
+    ana     a, {reg: reg_vabcdehl}  => $608 @ 0b1 @ reg
+    anax    [{reg: ldax_reg}]       => $708 @ 0b1 @ reg
+    ora     {reg: reg_vbcdehl}, a   => $601 @ 0b1 @ reg
+    ora     a, {reg: reg_vabcdehl}  => $609 @ 0b1 @ reg
+    orax    [{reg: ldax_reg}]       => $709 @ 0b1 @ reg
+    xra     {reg: reg_vbcdehl}, a   => $601 @ 0b0 @ reg
+    xra     a, {reg: reg_vabcdehl}  => $609 @ 0b0 @ reg
+    xrax    [{reg: ldax_reg}]       => $709 @ 0b0 @ reg
+    gta     {reg: reg_vbcdehl}, a   => $602 @ 0b1 @ reg
+    gta     a, {reg: reg_vabcdehl}  => $60A @ 0b1 @ reg
+    gtax    [{reg: ldax_reg}]       => $70A @ 0b1 @ reg
+    lta     {reg: reg_vbcdehl}, a   => $603 @ 0b1 @ reg
+    lta     a, {reg: reg_vabcdehl}  => $60B @ 0b1 @ reg
+    ltax    [{reg: ldax_reg}]       => $70B @ 0b1 @ reg
+    nea     {reg: reg_vbcdehl}, a   => $606 @ 0b1 @ reg
+    nea     a, {reg: reg_vabcdehl}  => $60E @ 0b1 @ reg
+    neax    [{reg: ldax_reg}]       => $70E @ 0b1 @ reg
+    eqa     {reg: reg_vbcdehl}, a   => $607 @ 0b1 @ reg
+    eqa     a, {reg: reg_vabcdehl}  => $60F @ 0b1 @ reg
+    eqax    [{reg: ldax_reg}]       => $70F @ 0b1 @ reg
 
-    ana     a, {reg: reg_vabcdehl} => $608 @ 0b1 @ reg
-    xra     a, {reg: reg_vabcdehl} => $609 @ 0b0 @ reg
-    ora     a, {reg: reg_vabcdehl} => $609 @ 0b1 @ reg
-    addnc   a, {reg: reg_vabcdehl} => $60A @ 0b0 @ reg
-    gta     a, {reg: reg_vabcdehl} => $60A @ 0b1 @ reg
-    subnb   a, {reg: reg_vabcdehl} => $60B @ 0b0 @ reg
-    lta     a, {reg: reg_vabcdehl} => $60B @ 0b1 @ reg
-    add     a, {reg: reg_vabcdehl} => $60C @ 0b0 @ reg
-    adc     a, {reg: reg_vabcdehl} => $60D @ 0b0 @ reg
-    sub     a, {reg: reg_vabcdehl} => $60E @ 0b0 @ reg
-    nea     a, {reg: reg_vabcdehl} => $60E @ 0b1 @ reg
-    sbb     a, {reg: reg_vabcdehl} => $60F @ 0b0 @ reg
-    eqa     a, {reg: reg_vabcdehl} => $60F @ 0b1 @ reg
+    ; Immediate Data Transfer (Accumulator)
+    xri    a, {value: u8}  => $16 @ value
+    adinc  a, {value: u8}  => $26 @ value
+    suinb  a, {value: u8}  => $36 @ value
+    adi    a, {value: u8}  => $46 @ value
+    aci    a, {value: u8}  => $56 @ value
+    sui    a, {value: u8}  => $66 @ value
+    sbi    a, {value: u8}  => $76 @ value
+    ani    a, {value: u8}  => $07 @ value
+    ori    a, {value: u8}  => $17 @ value
+    gti    a, {value: u8}  => $27 @ value
+    lti    a, {value: u8}  => $37 @ value
+    oni    a, {value: u8}  => $47 @ value
+    offi   a, {value: u8}  => $57 @ value
+    nei    a, {value: u8}  => $67 @ value
+    eqi    a, {value: u8}  => $77 @ value
 
-    anax    [{reg: ldax_reg}] => $708 @ 0b1 @ reg
-    xrax    [{reg: ldax_reg}] => $709 @ 0b0 @ reg
-    orax    [{reg: ldax_reg}] => $709 @ 0b1 @ reg
-    addncx  [{reg: ldax_reg}] => $70A @ 0b0 @ reg
-    gtax    [{reg: ldax_reg}] => $70A @ 0b1 @ reg
-    subnbx  [{reg: ldax_reg}] => $70B @ 0b0 @ reg
-    ltax    [{reg: ldax_reg}] => $70B @ 0b1 @ reg
-    addx    [{reg: ldax_reg}] => $70C @ 0b0 @ reg
-    adcx    [{reg: ldax_reg}] => $70D @ 0b0 @ reg
-    subx    [{reg: ldax_reg}] => $70E @ 0b0 @ reg
-    neax    [{reg: ldax_reg}] => $70E @ 0b1 @ reg
-    sbbx    [{reg: ldax_reg}] => $70F @ 0b0 @ reg
-    eqax    [{reg: ldax_reg}] => $70F @ 0b1 @ reg
+    ; Immediate Data Transfer (Special Register)
+    ani  {port: ani_port}, {value: u8} => $648 @ 0b10 @ port @ value
+    ori  {port: ani_port}, {value: u8} => $649 @ 0b10 @ port @ value
+    offi {port: ani_port}, {value: u8} => $64d @ 0b10 @ port @ value
+    oni  {port: ani_port}, {value: u8} => $64c @ 0b10 @ port @ value
 
-    ani a, {value: u8} => $07 @ value
-    xri a, {value: u8} => $16 @ value
-    ori a, {value: u8} => $17 @ value
-    adinc a, {value: u8} => $26 @ value
-    gti a, {value: u8} => $27 @ value
-    suinb a, {value: u8} => $36 @ value
-    lti a, {value: u8} => $37 @ value
-    adi a, {value: u8} => $46 @ value
-    oni a, {value: u8} => $47 @ value
-    aci a, {value: u8} => $56 @ value
-    offi a, {value: u8} => $57 @ value
-    sui a, {value: u8} => $66 @ value
-    nei a, {value: u8} => $67 @ value
-    sbi a, {value: u8} => $76 @ value
-    eqi a, {value: u8} => $77 @ value
-
-    ani     {reg: reg_vbcdehl}, {value: u8} => $640 @ 0b1 @ reg @ value
-    xri     {reg: reg_vbcdehl}, {value: u8} => $641 @ 0b0 @ reg @ value
-    ori     {reg: reg_vbcdehl}, {value: u8} => $641 @ 0b1 @ reg @ value
-    adinc   {reg: reg_vbcdehl}, {value: u8} => $642 @ 0b0 @ reg @ value
-    gti     {reg: reg_vbcdehl}, {value: u8} => $642 @ 0b1 @ reg @ value
-    suinb   {reg: reg_vbcdehl}, {value: u8} => $643 @ 0b0 @ reg @ value
-    lti     {reg: reg_vbcdehl}, {value: u8} => $643 @ 0b1 @ reg @ value
-    adi     {reg: reg_vbcdehl}, {value: u8} => $644 @ 0b0 @ reg @ value
-    oni     {reg: reg_vbcdehl}, {value: u8} => $644 @ 0b1 @ reg @ value
-    aci     {reg: reg_vbcdehl}, {value: u8} => $645 @ 0b0 @ reg @ value
-    offi    {reg: reg_vbcdehl}, {value: u8} => $645 @ 0b1 @ reg @ value
-    sui     {reg: reg_vbcdehl}, {value: u8} => $646 @ 0b0 @ reg @ value
-    nei     {reg: reg_vbcdehl}, {value: u8} => $646 @ 0b1 @ reg @ value
-    sbi     {reg: reg_vbcdehl}, {value: u8} => $647 @ 0b0 @ reg @ value
-    eqi     {reg: reg_vbcdehl}, {value: u8} => $647 @ 0b1 @ reg @ value
-
+    ; Working Register
     aniw    [{addr: hi_addr}], {value: u8} => $05 @ addr @ value
     oriw    [{addr: hi_addr}], {value: u8} => $15 @ addr @ value
     gtiw    [{addr: hi_addr}], {value: u8} => $25 @ addr @ value
@@ -220,45 +190,34 @@
     neiw    [{addr: hi_addr}], {value: u8} => $65 @ addr @ value
     eqiw    [{addr: hi_addr}], {value: u8} => $75 @ addr @ value
 
-    ani  {port: ani_port}, {value: u8} => $648 @ 0b10 @ port @ value
-    ori  {port: ani_port}, {value: u8} => $649 @ 0b10 @ port @ value
-    oni  {port: ani_port}, {value: u8} => $64c @ 0b10 @ port @ value
-    offi {port: ani_port}, {value: u8} => $64d @ 0b10 @ port @ value
+    ; Increment/Decrement
+    inr {reg: reg_inr} => $4 @ reg
+    inrw [{addr: hi_addr}] => $20 @ addr
+    dcr {reg: reg_inr} => $5 @ reg
+    dcrw [{addr: hi_addr}] => $30 @ addr
+    inx {reg: inx_reg} => reg @ $2
+    dcx {reg: inx_reg} => reg @ $3
 
-    sspd [{addr: u16}] => $700E @ le(addr)
-    lspd [{addr: u16}] => $700F @ le(addr)
-    sbcd [{addr: u16}] => $701E @ le(addr)
-    lbcd [{addr: u16}] => $701F @ le(addr)
-    sded [{addr: u16}] => $702E @ le(addr)
-    lded [{addr: u16}] => $702F @ le(addr)
-    shld [{addr: u16}] => $703E @ le(addr)
-    lhld [{addr: u16}] => $703F @ le(addr)
-    push {reg: push_reg} => $48 @ reg @ $e
-    pop {reg: push_reg} => $48 @ reg @ $f
+    ; Miscellaneous
+    daa => $61
+    stc => $482B
+    clc => $482A
 
+    ; Rotate and Shift
+    rld => $4838
+    rrd => $4839
+    ral => $4830
+    rar => $4831
+
+    ; Jump
     jmp {addr: u16} => $54 @ le(addr)
-    call {addr: u16} => $44 @ le(addr)
-
-    calf {addr: u16} => {
-        assert(addr >= 0x0800)
-        assert(addr <= 0x0FFF)
-        0b0111 @ addr`12
-    }
-
-    calt {addr: u16} => {
-        assert(addr & 0x0001 == 0)
-        assert(addr >= 0x0080)
-        assert(addr <= 0x00FE)
-        0b10 @ ((addr - 0x0080) >> 1)`6
-    }
-
+    jb => $73
     jr {addr: u16} => {
         reladdr = addr - $ - 1
         assert(reladdr <= 0x3f)
         assert(reladdr >= -0x3f)
         0b11 @ reladdr`6
     }
-
     jre {addr: u16} => {
         reladdr = addr - $ - 2
         assert(reladdr <= 0xff)
@@ -269,11 +228,49 @@
         )
     }
 
+    ; Call
+    call {addr: u16} => $44 @ le(addr)
+    calf {addr: u16} => {
+        assert(addr >= 0x0800)
+        assert(addr <= 0x0FFF)
+        0b0111 @ addr`12
+    }
+    calt {addr: u16} => {
+        assert(addr & 0x0001 == 0)
+        assert(addr >= 0x0080)
+        assert(addr <= 0x00FE)
+        0b10 @ ((addr - 0x0080) >> 1)`6
+    }
+
+    ; Return
+    ret => $08
+    rets => $18
+    reti => $62
+
+    ; Skip
+    skn cy   => $481A
+    skn z    => $481C
     sknit f0 => $4810
     sknit ft => $4811
     sknit f1 => $4812
     sknit f2 => $4813
     sknit fs => $4814
-    skn cy   => $481A
-    skn z    => $481C
+
+    ; CPU Control
+    nop => $00
+    ei  => $4820
+    di  => $4824
+
+    ; Serial Port Control
+    ; sio
+    stm => $19
+
+    ; Port E Control
+    pex => $482D
+    per => $483C
+
+    ; Data Directives
+    db {b: bytes} => b
+    dw {w: words} => w
+
 }
